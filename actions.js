1bf3b2974269cffa2e1f6f2cc1019e296a6ef67a
Jim Strother <jim.strother@gmail.com>
2016-06-15 17:25:30 -0700

    first commit

---
+++ actions.js
@@ -0,0 +1,9 @@
+// action needed for "What?" info screen
+
+// action needed for new game
+
+// action comparing numbers
+
+// action for displaying number of guess
+
+// action for displaying numbers already guessed
