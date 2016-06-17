278dc7e41cfca49e51ad1153a88ba83b8f0f8766
Jim Strother <jim.strother@gmail.com>
2016-06-16 17:23:35 -0700

    adding logic to reducers.js

--- reducers.js
+++ reducers.js
@@ -3,16 +3,24 @@ const actions = require('./actions.js');
 var initialState = [];
 
 var hotColdReducer = function(state, action) {
-	state = state || initialState;
+	var state = state || initialState;
+	var randomNumber;
 	switch (action.type) {
 		case actions.NEW_GAME:
+			(function(min, max) {
+				randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
+			})(1, 100);
 			return state.concat({
-				
+
 			});
 			break;
 		case actions.NUMBER_GUESS:
+			var guess = action.guess;
+			var guessCount = 0;
+			var prevGuess = [];
+			compareNumbers(guess, randomNumber);
 			return state.concat({
-				guess: guess
+
 			});
 			break;
 		case actions.INFO_SCREEN:
@@ -24,5 +32,32 @@ var hotColdReducer = function(state, action) {
 
 	return state;
 };
+
+function difference(num1, num2) {
+  return Math.abs((num1 - num2));
+}
+function compareNumbers(compare1, compare2) {
+  guessCount++;
+  prevGuess.push(' ' + userGuess);
+  $('#feedback p').text('');
+  if (difference(compare1, compare2) == 0) {
+    $('#feedback p').text('You got it in ' + guessCount + ' guesses! Great guess!');
+  } else {
+      if (difference(compare1, compare2) >= 60) {
+          $('#feedback p').text('Freezing');
+      } else if (difference(compare1, compare2) >= 45) {
+          $('#feedback p').text('Cold.');
+      } else if (difference(compare1, compare2) >= 35) {
+          $('#feedback p').text('So-so.');
+      } else if (difference(compare1,compare2) >= 15 ) {
+          $('#feedback p').text('Warmer!');
+      }else if (difference(compare1, compare2) >= 5) {
+          $('#feedback p').text('HOT HOT HOT!!!');
+      } else {
+          $('#feedback p').text('Almost standing on it!');
+      }
+  }
+  $('#guessCount').text(guessCount);
+}
 
 exports.hotColdReducer = hotColdReducer;
