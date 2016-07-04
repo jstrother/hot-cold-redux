c00e4395ce7a411a849642e06c5f7740bf006f0c
Jim Strother <jim.strother@gmail.com>
2016-07-04 16:58:13 -0700

    adding node.js API using mongo to store least number of guesses

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+var LeastGuesses = function() {
+	this.items = [];
+};
+// the plan is to keep this array restricted to containing a single number- the least number of guesses used to guess the hidden hot-cold number.  I only wish to change this value if the new value is smaller than the current value. The add method below should only be used once, otherwise, use the update method.
+LeastGuesses.prototype.add = function(guess) {
+	let item = {guess: guess};
+	this.items.push(item);
+	return item;
+};
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/guesses', function(request, response) {
+	response.json(guesses);
+});
+
+app.listen(8080);
