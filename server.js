660c46f904187f5e5ff22f9fd781b3624adb28bf
Jim Strother <strotherwebdev@gmail.com>
2016-10-27 03:42:33 +0000

    heroku push error, trying to fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const	jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 
 const Least = function() {
 	this.leastGuesses = 1000;
